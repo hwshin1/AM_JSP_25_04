59cab664cf137e8506dc8821009abe1c53fc669e
hwshin1 <hwshin5044@gmail.com>
2025-04-28 00:39:05 +0900

    리스트를 테이블로 표현, 게시글 삭제 기능 구현

---
+++ src/main/java/com/KoreaIT/java/AM_JSP/servlet/ArticleDeleteServlet.java
@@ -0,0 +1,66 @@
+package com.KoreaIT.java.AM_JSP.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Map;
+
+import com.KoreaIT.java.AM_JSP.util.DBUtil;
+import com.KoreaIT.java.AM_JSP.util.SecSql;
+
+@WebServlet("/article/doDelete")
+public class ArticleDeleteServlet extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8");
+		response.getWriter().append("실행");
+		
+		// DB 연결
+ 		try {
+ 			// cj 붙이기 최신은 cj 붙여야 한다.
+ 			Class.forName("com.mysql.cj.jdbc.Driver");
+ 		} catch (ClassNotFoundException e) {
+ 			System.out.println("클래스 x");
+ 			e.printStackTrace();
+ 		}
+		
+		String user = "root";
+		String password= "1234";
+		// serverTimezone 넣기 안넣으면 작동 안할수도 있음
+		String url = "jdbc:mysql://localhost:3306/AM_JSP_25_04?serverTimezone=Asia/Seoul&useUnicode=true&characterEncoding=utf8";
+		
+		Connection conn = null;
+		
+        try {
+        	conn = DriverManager.getConnection(url, user, password);
+        	response.getWriter().append("연결 성공!");
+        	
+        	DBUtil dbUtil = new DBUtil(request, response);
+        	
+        	int id = Integer.parseInt(request.getParameter("id"));
+        	 
+        	SecSql sql = SecSql.from("DELETE");
+ 			sql.append("FROM article");
+ 			sql.append("WHERE id = ?;", id);
+ 			
+ 			DBUtil.delete(conn, sql);
+        	
+ 			response.getWriter().append(String.format("<script>alert('%d번 글이 삭제되었습니다.'); location.replace('list');</script>", id));
+        } catch (SQLException e) {
+            System.out.println("에러 1 : " + e);
+        } finally {
+            try {
+                if (conn != null && !conn.isClosed()) {
+                    conn.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+	}
+}
--- src/main/webapp/jsp/article/list.jsp
+++ src/main/webapp/jsp/article/list.jsp
@@ -10,27 +10,45 @@ List<Map<String, Object>> articleRows = (List<Map<String, Object>>)request.getAt
 <head>
 <meta charset="UTF-8">
 <title>게시글 목록</title>
+<style>
+table>thead>tr>th, table>tbody>tr>td {
+ 	padding: 5px;
+ }
+</style>
 </head>
 <body>
 	<h2>게시글 목록</h2>
 
-	<a href="https://www.naver.com" target="_blank">네이버</a>
-	<a href="http://localhost:8080/AM_JSP_25_04/article/list"
-		target="_blank">리스트 새 탭</a>
+	<a href="../home/main">메인으로 이동</a>
 
-	<a href="http://localhost:8080/AM_JSP_25_04/article/detail"
-		target="_blank">디테일 새 탭</a>
-
-	<ul>
-		<%
-		for (Map<String, Object> articleRow : articleRows) {
-		%>
-		<li><%=articleRow.get("id")%>번,<%=articleRow.get("regDate")%>, <a
-			href="detail?id=<%=articleRow.get("id")%>"><%=articleRow.get("title")%></a>,<%=articleRow.get("body")%></li>
-		<%
-		}
-		%>
-
-	</ul>
+	<table style="border-collapse: collapse; border-color: green;"
+		border="1px">
+		<thead>
+			<tr>
+				<th>번호</th>
+				<th>날짜</th>
+				<th>제목</th>
+				<th>내용</th>
+				<th>삭제</th>
+			</tr>
+		</thead>
+		<tbody>
+			<%
+			for (Map<String, Object> articleRow : articleRows) {
+			%>
+			<tr style="text-align: center;">
+				<td><%=articleRow.get("id")%>번</td>
+				<td><%=articleRow.get("regDate")%></td>
+				<td><%=articleRow.get("title")%></td>
+				<td><%=articleRow.get("body")%></td>
+				<td><a
+					onclick="if ( confirm('정말 삭제하시겠습니까?') == false ) { return false; }"
+					href="doDelete?id=<%=articleRow.get("id")%>">del</a></td>
+			</tr>
+			<%
+			}
+			%>
+		</tbody>
+	</table>
 </body>
 </html>
